cf95072d88c78547f2ac38044d2e9c1cc06760a2
pseuyi <pseuyi@gmail.com>
2020-07-24 14:42:06 -0400

    bootstrap basic app functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+import useSWR from 'swr';
+import {useState} from 'react';
+import Problem from './Problem';
 import './App.css';
 
+const MP_KEY = process.env.REACT_APP_MP_API_KEY;
+
+const fetcher = (url: any) => fetch(url).then(r => r.json());
+
 function App() {
+  const [lat, setLat] = useState(40.03);
+  const [lon, setLon] = useState(-105.25);
+  const [problem, setProblem] = useState();
+
+  const {data, error} = useSWR(
+    `https://www.mountainproject.com/data/get-routes-for-lat-lon?lat=${lat}&lon=${lon}&maxDistance=50&minDiff=V0&maxDiff=V15&key=${MP_KEY}`,
+    fetcher,
+  );
+
+  if (error) return <div> failed to fetch</div>;
+  if (!data) return <div> loading...</div>;
+  console.log('data', data);
+
+  const onsuccess = (pos: any) => {
+    console.log('pos', pos);
+    setLat(pos.coords.latitude);
+    setLon(pos.coords.longitude);
+  };
+
+  const onerror = () => {
+    return <div>error</div>;
+  };
+
+  if (!navigator.geolocation) {
+    return <div>Geolocation is not supported by your browser</div>;
+  } else {
+    navigator.geolocation.getCurrentPosition(onsuccess, onerror);
+  }
+
+  const handleProblemClick = (route: any) => {
+    setProblem(route);
+  };
+
+  const showProblem = () => {
+    if (problem) return <Problem route={problem} />;
+    else return <></>;
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">bouldering videos</header>
+      <div>lat: {lat}</div>
+      <div>lon: {lon}</div>
+
+      <ul>
+        {data.routes.map((r: any, i: number) => (
+          <li onClick={() => handleProblemClick(r)} key={i}>
+            {r.name} * {r.rating} * {r.location[r.location.length - 1]}
+          </li>
+        ))}
+      </ul>
+      {showProblem()}
     </div>
   );
 }
---
+++ src/Problem.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import useSWR from 'swr';
+
+const YT_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
+const YT_URL = (q: string) =>
+  `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${q}&key=${YT_KEY}`;
+
+const YT_BASE = 'https://www.youtube.com/watch?v=';
+
+const fetcher = (url: any) => fetch(url).then(r => r.json());
+
+const Problem = ({route}: any) => {
+  const {data, error} = useSWR(
+    YT_URL(`${route.name} ${route.location[route.location.length - 1]}`),
+    fetcher,
+  );
+  console.log('route', route);
+  console.log('yt3data', data);
+
+  if (!route) return <div>loading...</div>;
+  if (error) return <div> failed to fetch</div>;
+  if (!data) return <div> loading...</div>;
+
+  const videoIds = data.items.map((d: any) => d.id.videoId);
+
+  return (
+    <div>
+      <img src={route.imgSqSmall}></img>
+      <p>{route.name}</p>
+      <p>{route.rating}</p>
+      <p>{route.location.join('-')}</p>
+      <div>videos</div>
+      <ul>
+        {data.items.map((v: any) => (
+          <li key={v.id.videoId}>
+            <a href={`${YT_BASE}${v.id.videoId}`}>{v.snippet.title}</a>
+          </li>
+        ))}
+      </ul>
+      {videoIds.slice(0, 5).map((id: string) => (
+        <iframe
+          id="player"
+          width="320"
+          height="195"
+          src={`http://www.youtube.com/embed/${id}?enablejsapi=1&origin=http://example.com`}></iframe>
+      ))}
+    </div>
+  );
+};
+
+export default Problem;
